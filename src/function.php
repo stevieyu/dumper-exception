23704cc3560d2f5c05e848ec75fb78d7ee513598
Stevie <yzm629@sina.cn>
2021-12-04 21:50:33 +0800

    update

--- src/function.php
+++ src/function.php
@@ -1,8 +1,19 @@
 <?php
 
+use Stvy\DumperException\Dumper\CliDumper;
+use Stvy\DumperException\Cloner\VarCloner;
+
 if (! function_exists('dd')) {
     function dd(...$vars)
     {
+        if(in_array(PHP_SAPI, ['cli', 'phpdbg'], true)){
+            $dumper = new CliDumper();
+            $cloner = new VarCloner();
+
+            foreach ($vars as $var){
+                $dumper->dump($cloner->cloneVar($var));
+            }
+        }
         throw new Stvy\DumperException\DdException($vars);
     }
 }
